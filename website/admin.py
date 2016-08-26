29370582436c59949849b4d04ce9ddad1e2230d8
auyer <rafa_auyer@icloud.com>
2016-08-26 18:42:50 -0300

    Models updated

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import com_aerea, cidade, voo
+ 
 # Register your models here.
+admin.site.register(com_aerea)
+admin.site.register(cidade)
+admin.site.register(voo)
