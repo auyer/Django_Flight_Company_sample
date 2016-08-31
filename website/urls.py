6d7dfc352a5c267503f1dfce6edb6e4987c1597d
auyer <rafa_auyer@icloud.com>
2016-08-31 19:33:01 -0300

    general Updates

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
+from .models import com_aerea,cidade,voo
 
 urlpatterns = [
     url(r'^$', views.home, name='Home'),
-    url(r'^$', views.voo_list, name='voo_list'),
-    url(r'^$', views.com_aerea_list, name='com_aerea_list'),
-    url(r'^$', views.city_list, name='city_list'),
+    url(r'^flights/$', views.voo_list, name='Full light List'),
+    url(r'^flight/no/(?P<pk>\d+)/$', views.voo_info, name='Flight Number '),
+    #url(r'^$', views.com_aerea_list, name='com_aerea_list'),
+    #url(r'^$', views.city_list, name='city_list'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
-from .models import com_aerea,cidade,voo
+from .models import com_aerea, cidade, voo
 
 # Create your views here.
 
 def home(request):
-    voo = voo.objects
     return render(request, 'website/index.html', {})
+    #voo = voo.objects.order_by('data_decolagem')
+    #return render(request, 'website/index.html', {'voo':voo})
 
 def voo_list(request):
-    return render(request, 'website/voo_list.html', {})
+    voo = voo.objects.order_by('data_decolagem')
+    return render(request, 'website/voo_list.html', {'voo':voo})
+
+def voo_info(request,pk):
+    voo = get_object_or_404(voo, pk=pk)
+    return render(request, 'website/voo_detail.html', {'voo':voo})
 
 def city_list(request):
     return render(request, 'website/city_list.html', {})
