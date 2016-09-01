aab94e74828f4574e1eb5ee968fab26661b9c559
Rafael Auyer <Auyer@Auyers-MacBook-Air-2.local>
2016-09-01 20:56:12 -0300

    Dynamic listing Page added

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.home, name='Home'),
     url(r'^flights/$', views.voo_list, name='Full light List'),
     url(r'^flight/(?P<pk>\d+)/$', views.voo_info, name='Flight Number '),
-    #url(r'^$', views.com_aerea_list, name='com_aerea_list'),
+    url(r'^twoflight/(?P<ctf>\w+)/(?P<ctt>\w+)$', views.advanced_Search, name='Two Way Flights'),
     #url(r'^$', views.city_list, name='city_list'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404, get_list_or_404
 import random
 from django.utils import timezone
 from .models import com_aerea, cidade, voo
+from django.db.models import Q
 
 # Create your views here.
 
@@ -18,3 +19,40 @@ def voo_list(request):
 def voo_info(request,pk):
     flight = get_object_or_404(voo, pk=pk)
     return render(request, 'website/voo_detail.html', {'flight':flight})
+
+
+
+
+
+
+
+
+
+
+def advanced_Search(request,ctf, ctt):
+    flight1= get_list_or_404(voo.objects.filter(id_origem = ctf).filter(id_destino = ctt))
+    flight2= get_list_or_404(voo.objects.filter(id_origem = ctt).filter(id_destino = ctf))
+
+    return render(request, 'website/two_voo_detail.html', {'flightF':flight1, 'flightT':flight2})
+
+def get_closest_to(self, target):
+    closest_greater_qs = self.filter(dt__gt=target).order_by('dt')
+    closest_less_qs    = self.filter(dt__lt=target).order_by('-dt')
+
+    try:
+        try:
+            closest_greater = closest_greater_qs[0]
+        except IndexError:
+            return closest_less_qs[0]
+
+        try:
+            closest_less = closest_less_qs[0]
+        except IndexError:
+            return closest_greater_qs[0]
+    except IndexError:
+        raise self.model.DoesNotExist("O percurso não existe")
+
+    if closest_greater.dt - target > target - closest_less.dt:
+        return closest_less
+    else:
+        return closest_greater
