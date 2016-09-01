bc9b899ef7f4d88ed5b5d1b13482d159331c3d62
evio <eviofragoso@outlook.com>
2016-09-01 16:49:50 -0300

    Search function added but not functional yet

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.home, name='Home'),
     url(r'^flights/$', views.voo_list, name='Full light List'),
     url(r'^flight/(?P<pk>\d+)/$', views.voo_info, name='Flight Number '),
-    #url(r'^$', views.com_aerea_list, name='com_aerea_list'),
-    #url(r'^$', views.city_list, name='city_list'),
+    url(r'^searchTest/$', views.search_form),
+    url(r'^sresults/$', views.search),
+   
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
 from .models import com_aerea, cidade, voo
@@ -17,4 +18,18 @@ def voo_info(request,pk):
     flight = get_object_or_404(voo, pk=pk)
     return render(request, 'website/voo_detail.html', {'flight':flight})
 
+def search_form(request):
+    return render(request, 'website/searchTest.html')
+
+def search(request):
+    error = False
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            flight = voo.objects.filter(id_destino_icontains=q)
+            return render(request, 'website/search_results.html',
+                {'flight': flight, 'query': q})
+    return render(request, 'website/search_results.html', {'error': error})
 
