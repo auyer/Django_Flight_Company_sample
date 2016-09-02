7c7f79bee06c419f8bd4083c273afa65b33f7fbb
evio <eviofragoso@outlook.com>
2016-09-02 14:19:38 -0300

    Added search engine to index

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^flights/$', views.voo_list, name='Full light List'),
     url(r'^flight/(?P<pk>\d+)/$', views.voo_info, name='Flight Number '),
     url(r'^login/$', views.login, name='Login'),
-    url(r'^promo/$', views.promo, name='Promoçoes'),
+    url(r'^promo/$', views.promo, name='Promocoes'),
     url(r'^searchTest/$', views.search_form),#link para a view que redireciona ao formulario de busca
     url(r'^sresults/$', views.search),#link para a view dos resultados da busca
     url(r'^twoflight/(?P<ctf>\w+)/(?P<ctt>\w+)$', views.advanced_Search, name='Two Way Flights'),
