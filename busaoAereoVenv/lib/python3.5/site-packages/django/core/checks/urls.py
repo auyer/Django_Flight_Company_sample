456fa388ded882b1d88a1839234157fedcbf8db3
Rafael Auyer <rafa_auyer@icloud.com>
2016-12-08 00:10:25 -0200

    moved gentelella files to static/gentelella

--- busaoAereoVenv/lib/python3.5/site-packages/django/core/checks/urls.py
+++ busaoAereoVenv/lib/python3.5/site-packages/django/core/checks/urls.py
@@ -95,10 +95,17 @@ def check_pattern_startswith_slash(pattern):
     Check that the pattern does not begin with a forward slash.
     """
     regex_pattern = pattern.regex.pattern
+    if not settings.APPEND_SLASH:
+        # Skip check as it can be useful to start a URL pattern with a slash
+        # when APPEND_SLASH=False.
+        return []
     if regex_pattern.startswith('/') or regex_pattern.startswith('^/'):
         warning = Warning(
-            "Your URL pattern {} has a regex beginning with a '/'. "
-            "Remove this slash as it is unnecessary.".format(describe_pattern(pattern)),
+            "Your URL pattern {} has a regex beginning with a '/'. Remove this "
+            "slash as it is unnecessary. If this pattern is targeted in an "
+            "include(), ensure the include() pattern has a trailing '/'.".format(
+                describe_pattern(pattern)
+            ),
             id="urls.W002",
         )
         return [warning]
--- busaoAereoVenv/lib/python3.5/site-packages/django/template/defaulttags.py
+++ busaoAereoVenv/lib/python3.5/site-packages/django/template/defaulttags.py
@@ -189,27 +189,19 @@ class ForNode(Node):
                 if unpack:
                     # If there are multiple loop variables, unpack the item into
                     # them.
-
-                    # To complete this deprecation, remove from here to the
-                    # try/except block as well as the try/except itself,
-                    # leaving `unpacked_vars = ...` and the "else" statements.
-                    if not isinstance(item, (list, tuple)):
-                        len_item = 1
-                    else:
+                    try:
                         len_item = len(item)
+                    except TypeError:  # not an iterable
+                        len_item = 1
                     # Check loop variable count before unpacking
                     if num_loopvars != len_item:
                         raise ValueError(
                             "Need {} values to unpack in for loop; got {}. "
                             .format(num_loopvars, len_item),
                         )
-                    try:
-                        unpacked_vars = dict(zip(self.loopvars, item))
-                    except TypeError:
-                        pass
-                    else:
-                        pop_context = True
-                        context.update(unpacked_vars)
+                    unpacked_vars = dict(zip(self.loopvars, item))
+                    pop_context = True
+                    context.update(unpacked_vars)
                 else:
                     context[self.loopvars[0]] = item
 
